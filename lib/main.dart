f944b048f77889581ad4332bb73ef4ef6adb53ac
amit <amit.singh.9312687983@gmail.com>
2024-05-03 22:57:23 +0530

    project initialise

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:startup_saathi/firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Startup Saathi',
+      theme: ThemeData.dark(),
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.dark,
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home Page"),
+        centerTitle: true,
+      ),
+    );
+  }
+}
